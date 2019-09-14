e591373d9d9027dc1f6d1b3dfbf2e7af656a9605
Suyash <suyash93@protonmail.com>
2019-09-14 12:00:42 +0530

    gcp

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages
+from setuptools import setup
+
+REQUIRED_PACKAGES = [
+    "tensorflow-gpu==2.0.0b1", "tensorflow-datasets==1.1.0",
+    "tf_sentencepiece==0.1.83"
+]
+
+setup(
+    name="trainer",
+    version="0.1",
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(),
+    include_package_data=True,
+    description="My Training Package",
+)
